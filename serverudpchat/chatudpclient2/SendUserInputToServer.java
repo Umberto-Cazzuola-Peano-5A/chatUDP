7cc32eb97a8d2e17ddd451a454325ae8b45ecbdd
Umberto-Cazzuola-Peano-5A <56472318+Umberto-Cazzuola-Peano-5A@users.noreply.github.com>
2019-10-31 12:31:58 +0100

    Add files via upload

---
+++ serverudpchat/chatudpclient2/SendUserInputToServer.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatudpclient;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Umberto Cazzuola 
+ */
+public class SendUserInputToServer implements Runnable {
+    DatagramSocket socket;
+    InetAddress address;
+    int UDP_port;
+    String username;
+    
+    SendUserInputToServer(DatagramSocket socket, InetAddress address, int UDP_port) {
+        this.socket = socket;
+        this.address = address;
+        this.UDP_port = UDP_port;
+    }
+    /**
+     *
+     */
+    @Override
+    public void run() {
+
+        byte[] buffer;
+        String messaggio;
+        Scanner tastiera = new Scanner(System.in);
+        DatagramPacket userDatagram;
+        System.out.println("Inserisci username > ");
+        username = tastiera.nextLine();
+
+        try {
+            System.out.print("> ");
+            do {
+                //Leggo da tastiera il messaggio utente vuole inviare
+                messaggio = username + " > " + tastiera.nextLine();
+
+                //Trasformo in array di byte la stringa che voglio inviare
+                buffer = messaggio.getBytes("UTF-8");
+
+                // Costruisco il datagram (pacchetto UDP) di richiesta 
+                // specificando indirizzo e porta del server a cui mi voglio collegare
+                // e il messaggio da inviare che a questo punto si trova nel buffer
+                userDatagram = new DatagramPacket(buffer, buffer.length, address, UDP_port);
+                // spedisco il datagram
+                socket.send(userDatagram);
+            } while (messaggio.compareTo("quit") != 0); //se utente digita quit il tread termina
+        } catch (IOException ex) {
+            Logger.getLogger(ChatUDPClient2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
